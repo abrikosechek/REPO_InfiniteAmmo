398032d6fdef213ef6550dfdf712931213764d5b
apelsin apelsin <abrikosechek@gmail.com>
2025-05-03 10:02:58 +0400

    removed commented code

---
+++ develop/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace InfiniteAmmo;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
+
+[HarmonyPatch(typeof(ItemGun), "Update")]
+public class ItemGunPatch {
+    public static void Prefix(ItemGun __instance)
+    {
+        __instance.batteryDrain = 0f;
+        __instance.itemBattery.batteryLife = 9999f;
+        __instance.itemBattery.batteryLifeInt = 9999;
+    }
+}
